ba04ec2b116bd84deb95a88ff5396a4c1cd7fb88
kelin.tan <kelin.tan@alo7.com>
2023-05-12 14:58:49 +0800

    support json path test

--- src/test/java/com/github/kelin/archetype/controller/GreetingControllerV2Test.java
+++ src/test/java/com/github/kelin/archetype/controller/GreetingControllerV2Test.java
@@ -2,6 +2,8 @@ package com.github.kelin.archetype.controller;
 
 import static com.github.kelin.archetype.TestConstants.USER_DATA;
 import static com.github.kelin.archetype.TestConstants.USER_V2_DATA;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +13,6 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,16 +27,22 @@ public class GreetingControllerV2Test {
     @Test
     void testGreeting() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/v2/greeting").param("id", "1"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json("{'id':1,'name':'test'}"));
     }
 
     @Test
     void testGreeting2() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/v2/greeting2").param("id", "1"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json("{'id':1,'name':'test_v2'}"));
+    }
+
+    @Test
+    void testGreeting3() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/v2/greeting3").param("id", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.name").value("test"));
     }
 }
