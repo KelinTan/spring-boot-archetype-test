3793e0df1ad5a940a3e9c5bd005f5eff555b5024
Kelin Tan <kelin.tan@alo7.com>
2023-07-24 10:34:41 +0800

    Refactor test scripts to load JSON data instead of BSON data

--- src/test/java/com/github/kelin/archetype/support/MongoScriptsTestExecutionListener.kt
+++ src/test/java/com/github/kelin/archetype/support/MongoScriptsTestExecutionListener.kt
@@ -2,16 +2,21 @@
 
 package com.github.kelin.archetype.support
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.bson.Document
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.stereotype.Component
 import org.springframework.test.context.TestContext
 import org.springframework.test.context.TestContextAnnotationUtils
 import org.springframework.test.context.support.AbstractTestExecutionListener
 import org.springframework.test.context.util.TestContextResourceUtils
+import java.io.File
 import java.nio.file.Files
 
 @Component
 class MongoScriptsTestExecutionListener : AbstractTestExecutionListener() {
+    private val objectMapper = ObjectMapper()
+
     override fun beforeTestMethod(testContext: TestContext) {
         val testClass = testContext.testClass
         val mongoTemplate = testContext.applicationContext.getBean(MongoTemplate::class.java)
@@ -22,8 +27,7 @@ class MongoScriptsTestExecutionListener : AbstractTestExecutionListener() {
             TestContextResourceUtils.convertToResourceList(
                 testContext.applicationContext, *scripts
             ).forEach {
-                val lines = Files.readAllLines(it.file.toPath())
-                mongoTemplate.insert(lines, it.file.nameWithoutExtension)
+                loadScript(it.file, mongoTemplate)
             }
         }
     }
@@ -31,7 +35,36 @@ class MongoScriptsTestExecutionListener : AbstractTestExecutionListener() {
     override fun afterTestMethod(testContext: TestContext) {
     }
 
+    private fun loadScript(script: File, mongoTemplate: MongoTemplate) {
+        if (script.name.endsWith(EXT_BSON)) {
+            val lines = Files.readAllLines(script.toPath())
+            mongoTemplate.insert(lines, script.nameWithoutExtension)
+        } else if (script.name.endsWith(EXT_JSON)) {
+            val scriptContent = script.readText().trim()
+            if (scriptContent.startsWith("[") && scriptContent.endsWith("]")) {
+                val documents = objectMapper.readValue<List<Document>>(
+                    script.readText(),
+                    objectMapper.typeFactory.constructCollectionType(
+                        List::class.java, Document::class.java
+                    )
+                )
+                mongoTemplate.insert(documents, script.nameWithoutExtension)
+            } else {
+                val document = objectMapper.readValue(script.readText(), Document::class.java)
+                mongoTemplate.insert(document, script.nameWithoutExtension)
+            }
+        } else {
+            // TODO support other script type
+            throw IllegalArgumentException("Unsupported script type: ${script.name}")
+        }
+    }
+
     private fun getScriptAnnotaions(clazz: Class<*>): Set<MongoScript> {
         return TestContextAnnotationUtils.getMergedRepeatableAnnotations(clazz, MongoScript::class.java)
+    }
+
+    companion object {
+        private const val EXT_BSON = "bson"
+        private const val EXT_JSON = "json"
     }
 }
--- src/test/java/com/github/kelin/archetype/support/TestConstants.java
+++ src/test/java/com/github/kelin/archetype/support/TestConstants.java
@@ -3,8 +3,8 @@ package com.github.kelin.archetype.support;
 public class TestConstants {
     public static final String USER_SQL = "classpath:sql/user.sql";
     public static final String USER_V2_SQL = "classpath:sql/user_v2.sql";
-    public static final String CUSTOMER_BSON = "classpath:mongo/customers.bson";
+    public static final String CUSTOMER_JSON = "classpath:mongo/customers.json";
     public static final String CUSTOMER_RECORD_BSON = "classpath:mongo/customer_records.bson";
     public static final String CUSTOMER_EXTRA_BSON = "classpath:mongo/customer_extras.bson";
-    public static final String USER_BSON = "classpath:mongo/user.bson";
+    public static final String USER_JSON = "classpath:mongo/user.json";
 }
